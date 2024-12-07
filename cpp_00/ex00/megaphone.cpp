b176bd83ae3e7c4614aa3fba0c7b72a5baf0ea84
souleeater <souleeater@kali.kali>
2024-12-07 23:27:22 +0100

    => write in c00

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+
+    i = 1;
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        while (i < ac)
+        {
+            j = 0;
+            while (av[i][j])
+                std::cout << (char)toupper(av[i][j++]);
+            i++;
+        }
+    }
+    std::cout << "\n";
+}
