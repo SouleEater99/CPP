aaa513681753b79453ea4d3cc8acbbcb4fe8bf17
SouleEater99 <rimox.midox2011@gmail.com>
2025-02-02 17:44:31 +0100

    ==> [C02/ex00] Class Created

--- CPP_02/ex00/Fixed.cpp
+++ CPP_02/ex00/Fixed.cpp
@@ -1,4 +1,30 @@
 #include "./Fixed.hpp"
 
 
+Fixed::Fixed(int value = 0) : _FixValue(value) {}
 
+Fixed::Fixed(Fixed& copy)
+{
+    setRawBits(copy._FixValue);
+}
+
+
+Fixed   Fixed::operator=(Fixed& copy) 
+{
+    return Fixed(copy);
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    return (_FixValue);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    _FixValue = raw;
+}
--- CPP_02/ex00/Fixed.hpp
+++ CPP_02/ex00/Fixed.hpp
@@ -1,6 +1,21 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
+class Fixed
+{
+private:
+    int _FixValue;
+    static const int _FracBits = 8;
+public:
+    Fixed(int value);
+    Fixed(Fixed& copy);
+    Fixed   operator=(Fixed& copy);
+    ~Fixed();
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+};
 
 
 #endif
