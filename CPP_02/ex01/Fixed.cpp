f4133f4d55a1bc515223e927bf676cd34f0fe902
SouleEater99 <rimox.midox2011@gmail.com>
2025-02-04 20:10:02 +0100

    ==> [C02/ex01] almost done

---
+++ CPP_02/ex01/Fixed.cpp
@@ -0,0 +1,65 @@
+#include "./Fixed.hpp"
+
+Fixed::Fixed(): _FixValue(0) 
+{
+    std::cout << "Default constructor called" << std::endl;
+} 
+
+Fixed::Fixed(const int value) 
+{
+    std::cout << "Int constructor called" << std::endl;
+    _FixValue = value << _FracBits;
+}
+
+Fixed::Fixed(const float value) 
+{
+    std::cout << "Float constructor called" << std::endl;
+    _FixValue = value * (1 << _FracBits);
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
+}
+
+
+Fixed&   Fixed::operator=(const Fixed& copy) 
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->_FixValue = copy.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (_FixValue);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    _FixValue = raw;
+}
+
+float   Fixed::toFloat( void ) const
+{
+    return _FixValue / (1 << _FracBits);
+}
+
+
+int     Fixed::toInt( void ) const
+{
+    return _FixValue >> _FracBits;
+}
+
+std::ostream&    operator << (std::ostream& out, const Fixed& other)
+{
+    out << other.toFloat();
+    return out;
+}
---
+++ CPP_02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+    int _FixValue;
+    static const int _FracBits = 8;
+public:
+    Fixed();
+    Fixed(const int value);
+    Fixed(const float value);
+    Fixed(const Fixed& copy);
+    Fixed&   operator=(const Fixed& copy);
+    ~Fixed();
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+    float toFloat( void ) const;
+    int toInt( void ) const;
+    friend std::ostream&    operator << (std::ostream& out, const Fixed& other);
+};
+
+
+#endif
