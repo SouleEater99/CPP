868cbeff875c2a2ad15afebc01c362a9d5b163c9
SouleEater99 <rimox.midox2011@gmail.com>
2025-02-05 19:13:03 +0100

    ==> [C02/ex02]  need to fixed precision

---
+++ CPP_02/ex02/Fixed.cpp
@@ -0,0 +1,128 @@
+#include "./Fixed.hpp"
+
+Fixed::Fixed(): _FixValue(0) 
+{
+    std::cout << "Default constructor called" << std::endl;
+} 
+
+Fixed::Fixed(const int value) 
+{
+    std::cout << "Int constructor called" << std::endl;
+    _FixValue = value << _FracBits;
+}
+
+Fixed::Fixed(const float value) 
+{
+    std::cout << "Float constructor called" << std::endl;
+    _FixValue = roundf(value * (1 << _FracBits));
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
+}
+
+
+Fixed&   Fixed::operator=(const Fixed& copy) 
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->_FixValue = copy.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (_FixValue);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    _FixValue = raw;
+}
+
+float   Fixed::toFloat( void ) const
+{
+    return (float)_FixValue / (1 << _FracBits);
+}
+
+
+int     Fixed::toInt( void ) const
+{
+    return _FixValue >> _FracBits;
+}
+
+std::ostream&    operator << (std::ostream& out, const Fixed& other)
+{
+    out << other.toFloat();
+    return out;
+}
+
+bool    Fixed::operator > (const Fixed& other)
+{
+    if (this->_FixValue > other._FixValue)
+        return true;
+    return false;
+}
+
+bool    Fixed::operator < (const Fixed& other)
+{
+    if (this->_FixValue < other._FixValue)
+        return true;
+    return false;
+}
+
+bool    Fixed::operator >= (const Fixed& other)
+{
+    if (this->_FixValue >= other._FixValue)
+        return true;
+    return false;
+}
+
+bool    Fixed::operator <= (const Fixed& other)
+{
+    if (this->_FixValue <= other._FixValue)
+        return true;
+    return false;
+}
+
+bool    Fixed::operator == (const Fixed& other)
+{
+    if (this->_FixValue == other._FixValue)
+        return true;
+    return false;
+}
+
+bool    Fixed::operator != (const Fixed& other)
+{
+    if (this->_FixValue != other._FixValue)
+        return true;
+    return false;
+}
+
+
+Fixed  Fixed::operator + (const Fixed& other)
+{
+    return (Fixed(this->toFloat() + other.toFloat()));
+}
+
+Fixed  Fixed::operator - (const Fixed& other)
+{
+    return (Fixed(this->toFloat() - other.toFloat()));
+}
+
+Fixed  Fixed::operator * (const Fixed& other)
+{
+    return (Fixed(this->toFloat() * other.toFloat()));
+}
+
+Fixed  Fixed::operator / (const Fixed& other)
+{
+    return (Fixed(this->toFloat() / other.toFloat()));
+}
---
+++ CPP_02/ex02/Fixed.hpp
@@ -0,0 +1,37 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+class Fixed
+{
+private:
+    int _FixValue;
+    static const int _FracBits = 8;
+public:
+    Fixed();
+    Fixed(const int value);
+    Fixed(const float value);
+    Fixed(const Fixed& copy);
+    Fixed&   operator=(const Fixed& copy);
+    ~Fixed();
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+    float toFloat( void ) const;
+    int toInt( void ) const;
+    friend std::ostream&    operator << (std::ostream& out, const Fixed& other);
+    bool    operator > (const Fixed& other);
+    bool    operator < (const Fixed& other);
+    bool    operator >= (const Fixed& other);
+    bool    operator <= (const Fixed& other);
+    bool    operator == (const Fixed& other);
+    bool    operator != (const Fixed& other);
+    Fixed  operator + (const Fixed& other);
+    Fixed  operator - (const Fixed& other);
+    Fixed  operator / (const Fixed& other);
+    Fixed  operator * (const Fixed& other);
+    
+};
+
+
+#endif
