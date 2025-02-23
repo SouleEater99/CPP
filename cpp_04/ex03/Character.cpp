db77558f7bcf56d649c397281e063ee8f50376e0
SouleEater99 <rimox.midox2011@gmail.com>
2025-02-23 13:53:24 +0000

    ==> [C04/ex03]  still i need handle the inventory

--- cpp_04/ex03/Character.cpp
+++ cpp_04/ex03/Character.cpp
@@ -1,12 +1,13 @@
 #include "./Character.hpp"
 
-Character::Character(): _name("Abdelali")
+Character::Character(): _name("Abdelali"), _inv_size(0)
 {
+
     std::cout << "Character: Default Constructor Called\n";
 }
 
 
-Character::Character(const std::string& name): _name(name)
+Character::Character(const std::string& name): _name(name), _inv_size(0)
 {
     std::cout << "Character: Parametrized Constructor Called\n";
 }
@@ -15,8 +16,14 @@ Character::Character(const std::string& name): _name(name)
 Character::Character(const Character& other): _name(other._name)
 {
     for (int i = 0; i < 4; i++)
+    {
         if (other._invetory[i])
+        {
             this->_invetory[i] = other._invetory[i]->clone();
+            this->set_addr(this->_invetory[i]);
+        }
+    }
+    this->_inv_size = other._inv_size;
     std::cout << "Character: Copy Constructor Called\n";
 }
 
@@ -29,8 +36,14 @@ Character&  Character::operator=(const Character& other)
 {
     this->_name = other._name;
     for (int i = 0; i < 4; i++)
+    {
         if (other._invetory[i])
+        {
             this->_invetory[i] = other._invetory[i]->clone();
+            this->set_addr(this->_invetory[i]);
+        }
+    }
+    this->_inv_size = other._inv_size;
 }
 
 std::string const    &Character::getName() const
@@ -40,12 +53,56 @@ std::string const    &Character::getName() const
 
 void Character::equip(AMateria *m)
 {
-
+    if (_inv_size >= 4)
+        std::cout << "Inventory is Full(use unequip method to get a place)\n";
+    else
+    {
+        if (m)
+        {
+            _invetory[_inv_size++] = m;
+            std::cout << m->getType() << " is equiped\n";
+        }
+    }
 }
 
 void Character::unequip(int idx)
-{}
+{
+    if (_inv_size == 0)
+        std::cout << "There is nothing in the Inventory\n";
+    else if (idx >= 0 && idx <= 3)
+        _invetory[idx] = NUll
+
+}
 
 
 void Character::use(int idx, ICharacter &target)
-{}
+{
+
+    
+}
+
+void Character::set_addr(AMateria* addr)
+{
+    int i;
+
+    i = 0;
+    while (i < 100 && _addr_saver[i] != NULL)
+        i++;
+    if (i >= 100)
+    {
+        clear_addr();
+        i = 0;
+    }
+    _addr_saver[i] = addr;
+
+}
+
+
+void Character::clear_addr()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        delete _addr_saver[i];
+        _addr_saver[i] = NULL;
+    }
+}
--- cpp_04/ex03/Character.hpp
+++ cpp_04/ex03/Character.hpp
@@ -8,6 +8,8 @@ class Character
 private:
     std::string _name;
     AMateria    *_invetory[4] = {NULL};
+    AMateria    *_addr_saver[100] = {NULL};
+    int         _inv_size; 
 public:
     Character();
     Character(const std::string& name);
@@ -18,6 +20,8 @@ public:
     void equip(AMateria *m);
     void unequip(int idx);
     void use(int idx, ICharacter &target);
+    void set_addr(AMateria* addr);
+    void clear_addr();
 };
 
 
