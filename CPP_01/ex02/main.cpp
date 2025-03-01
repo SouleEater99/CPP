df10c3787f9c90b69d639823053c41f42f50b11b
Abdelali El Maimouni <rimox.midox2011@gmail.com>
2025-03-01 07:03:53 -0500

    ==> cpp01/ex02 just make fclean, and push again

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -9,7 +9,7 @@ int main()
 
     std::cout << "The memory address of the string variable : " << &message << std::endl;
     std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
-    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl << std::endl;
 
     std::cout << "The value of the string variable: " << message << std::endl;   
     std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;   
