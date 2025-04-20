cf97cc00809b0c3b6fa3e4a7080940f9e8cb587b
SouleEater99 <rimox.midox2011@gmail.com>
2025-04-20 08:23:29 +0100

    [CPP06/ex00] In Creating Process

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -1,3 +1,33 @@
 #include "./ScalarConverter.hpp"
 
+ScalarConverter::ScalarConverter()
+{
+    std::cout << "ScalarConverter: Constructor Called" << std::endl;
+}
 
+ScalarConverter::ScalarConverter(const ScalarConverter& other)
+{
+    std::cout << "ScalarConverter: Copy Constructor Called" << std::endl;
+}
+
+ScalarConverter::~ScalarConverter()
+{
+    std::cout << "ScalarConverter: Destructor Called" << std::endl;
+}
+
+ScalarConverter&    ScalarConverter::operator=(const ScalarConverter& other)
+{
+    if (this == &other)
+        return *this;
+    return *this;
+}
+
+bool    ScalarConverter::_CheckScalarType(const std::string& str)
+{
+
+}
+
+void    ScalarConverter::convert(const std::string& str)
+{
+
+}
--- cpp_06/ex00/ScalarConverter.hpp
+++ cpp_06/ex00/ScalarConverter.hpp
@@ -1,8 +1,21 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
+#include <iostream>
+#include <string>
 
+class ScalarConverter
+{
+private:
+    bool    _CheckScalarType(const std::string& str);
+public:
+    ScalarConverter();
+    ScalarConverter(const ScalarConverter& other);
+    ~ScalarConverter();
+    ScalarConverter&    operator = (const ScalarConverter& other);
+    void    convert(const std::string& str);
 
-
+    
+};
 
 #endif
