dfaf50c32d762e56ab2633362ab4e2ae9e877c5e
SouleEater99 <rimox.midox2011@gmail.com>
2025-04-20 12:33:06 +0100

    [CPP06/ex00] In Creating Process

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -22,12 +22,75 @@ ScalarConverter&    ScalarConverter::operator=(const ScalarConverter& other)
     return *this;
 }
 
-bool    ScalarConverter::_CheckScalarType(const std::string& str)
+void    ScalarConverter::convert(const std::string& str)
 {
 
 }
 
-void    ScalarConverter::convert(const std::string& str)
+bool    ScalarConverter::IsChar(const std::string& str)
 {
+    if (str.length() == 3 && str[0] == '\'' && str[2] == '\'')
+        if (isalpha(str[1]))
+            return (true);
+    return (false);
+}
 
+bool    ScalarConverter::IsInt(const std::string& str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (i == 0 && (str[i] == '-' || str[i] == '+'))
+            continue;
+        if (!isdigit(str[i]))
+            return false;
+    }
+    return true;
+}
+
+bool    ScalarConverter::IsFloat(const std::string& str)
+{
+    int i;
+    int n_dot;
+    
+    i = 0; 
+    n_dot = 0;
+
+    if (str[i] == '.')
+        return false;
+    if (i == 0 && (str[i] == '-' || str[i] == '+'))
+        i++;
+    while (i < str.length() - 1)
+    {
+        if (str[i] == '.' && str[i + 1] && str[i + 1] != 'f')
+            n_dot++;
+        else if (!isdigit(str[i++]))
+            return false;
+    }
+    if (n_dot > 1 || !str[i] == 'f')
+        return false;
+    return true;
+}
+
+bool    ScalarConverter::IsDouble(const std::string& str)
+{
+    int i;
+    int n_dot;
+    
+    i = 0; 
+    n_dot = 0;
+
+    if (str[i] == '.')
+        return false;
+    if (i == 0 && (str[i] == '-' || str[i] == '+'))
+        i++;)
+    while (i < str.length() - 1)
+    {
+        if (str[i] == '.' && str[i + 1])
+            n_dot++;
+        else if (!isdigit(str[i++]))
+            return false;
+    }
+    if (n_dot > 1)
+        return false;
+    return true;
 }
--- cpp_06/ex00/ScalarConverter.hpp
+++ cpp_06/ex00/ScalarConverter.hpp
@@ -7,15 +7,26 @@
 class ScalarConverter
 {
 private:
-    bool    _CheckScalarType(const std::string& str);
+    int     ScalarConverter::*_PrintLiteralType(const std::string& str);
+    int     _PrintByChar(const std::string& str);
+    int     _PrintByInt(const std::string& str);
+    int     _PrintByFloat(const std::string& str);
+    int     _PrintByDouble(const std::string& str);
 public:
     ScalarConverter();
     ScalarConverter(const ScalarConverter& other);
     ~ScalarConverter();
     ScalarConverter&    operator = (const ScalarConverter& other);
     void    convert(const std::string& str);
+    bool    IsChar(const std::string& other);
+    bool    IsInt(const std::string& other);
+    bool    IsFloat(const std::string& other);
+    bool    IsDouble(const std::string& other);
+
 
     
 };
 
+
+    
 #endif
