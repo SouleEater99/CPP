f7577bf4b11f1b7b25581f280d9e4e8cc389c065
SouleEater99 <rimox.midox2011@gmail.com>
2025-10-01 14:14:53 +0100

    we are done with CPP08i/Ex02

---
+++ cpp_08/ex02/MutantStack.hpp
@@ -0,0 +1,27 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <iostream>
+#include <deque>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+private:
+    /* data */
+public:
+    MutantStack(/* args */);
+    MutantStack(const MutantStack &other);
+    MutantStack& operator=(const MutantStack &other);
+    ~MutantStack();
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    
+    iterator begin();
+    iterator end();
+};
+
+
+#include "./MutantStack.tpp"
+
+#endif
